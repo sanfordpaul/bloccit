040d6f436fa45c926e5723e90a4476f6ccd744bf
Paul Sanford <sanfordpaul@gmail.com>
2017-07-20 08:17:13 -0500

    DB migrations changed, question comes before answer

--- db/migrate/20170703210401_create_answers.rb
+++ db/migrate/20170703210401_create_answers.rb
@@ -2,7 +2,7 @@ class CreateAnswers < ActiveRecord::Migration[5.1]
   def change
     create_table :answers do |t|
       t.text :body
-      # t.references :question, index: true, foreign_key: true
+      t.references :question, index: true, foreign_key: true
 
       t.timestamps null: false
     end
