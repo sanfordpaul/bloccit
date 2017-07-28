01c5d0bb006f6193da853b53fe5d914116be244f
Paul Sanford <sanfordpaul@gmail.com>
2017-07-28 15:57:49 -0500

    checkpoint 32 completed

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -7,11 +7,13 @@ class Comment < ApplicationRecord
 
   after_create :send_favorite_emails
 
+  default_scope { order('updated_at DESC') }
+  
   private
 
   def send_favorite_emails
     post.favorites.each do |favorite|
       FavoriteMailer.new_comment(favorite.user, post, self).deliver_now
     end
-  end  
+  end
 end
