9ab9897baf4ab0fb44ac699b55c6091216e870a1
Paul Sanford <sanfordpaul@gmail.com>
2017-08-02 15:33:25 -0500

    assignment 33 completed

--- app/controllers/api/v1/topics_controller.rb
+++ app/controllers/api/v1/topics_controller.rb
@@ -10,6 +10,7 @@ class Api::V1::TopicsController < Api::V1::BaseController
 
   def show
     topic = Topic.find(params[:id])
-    render json: topic, status: 200
+    posts = Post.where(topic_id: params[:id])
+    render json: [topic, posts], status: 200
   end
 end
